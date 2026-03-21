79673f30039f43e606dea4ba9189f52b3cb23189
YourName <youremail@gmail.com>
2026-03-21 13:59:15 +0530

    fixed env variables for production

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+jsimport { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://cxgwfnuicystmiokyzcz.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4Z3dmbnVpY3lzdG1pb2t5emN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwNzIxMjUsImV4cCI6MjA4OTY0ODEyNX0.GIDPHJTlGuMIQlRhEE4D5CgOD54Fm7NpLknWtPnQNpo'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
